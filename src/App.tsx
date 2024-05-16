f4b50b94df9946aa409785547b835882f77a5f3a
Arun <youngdeveloper37@gmail.com>
2024-05-16 15:37:48 +0530

    FIX :: update App and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Header } from "./components/Header.component";
+import { UserInput } from "./components/UserInput.component";
+import { Logs } from "./components/logs.component";
 
 export const App: FC = () => {
+  
+  const [inputValues, setInputValues] = useState<{
+    initial: number;
+    annual: number;
+    returns: number;
+    duration: number;
+  }>({ initial: 10000, annual: 1200, returns: 6, duration: 10 });
+
+  const onChangeHandler = (key: string, value: number) => {
+    setInputValues((prevValue) => {
+      return { ...prevValue, [key]: value };
+    });
+  };
+
+  
+
+ 
   return (
     <>
       <Header />
+      <UserInput onChange={onChangeHandler} value={inputValues} />
+      <Logs inputValues={inputValues} />
     </>
   );
 };
